62870f4276017a9e6c3808f910d5e9b52cee3024
abdi <114880114+abdirahimAli@users.noreply.github.com>
2023-03-17 16:56:04 +0000

    first comment

---
+++ galleria-slideshow-site/starter-code/index.js
@@ -0,0 +1,77 @@
+
+ const  Drawing1 = document.getElementById('starry_night');
+ const  Drawing2 = document.getElementById('GirlwithaPearlEarring');
+ const  Drawing3 = document.getElementById('Guernica');
+ const  Drawing4 = document.getElementById('PenitentMagdalene');
+ const  Drawing5 = document.getElementById('TheStormontheSeaofGalilee');
+ const  Drawing6 = document.getElementById('TheGreatWaveoffKanagawa');
+ const  Drawing7 = document.getElementById('VanGoghSelf-portrait');
+ const  Drawing8 = document.getElementById('TheSleepingGypsy');
+ const  Drawing9 = document.getElementById('LadywithanErmine');
+ const  Drawing10 = document.getElementById('TheNightCafé');
+ const  Drawing11 = document.getElementById('MonaLisa');
+ const  Drawing12 = document.getElementById('TheBasketofApples');
+ const  Drawing13 = document.getElementById('TheBoyintheRedVest');
+ const  Drawing14 = document.getElementById('ArnolfiniPortrait');
+ const  Drawing15 = document.getElementById('TheSwing');
+
+ Drawing1.addEventListener('click',()=>{
+
+   window.location.href = "./starry-night.html";
+})
+ Drawing2.addEventListener('click',()=>{
+
+    window.location.href = "./girl-with-pearl-earring.html";
+ })
+ Drawing3.addEventListener('click',()=>{
+
+   window.location.href = "./guernica.html";
+})
+Drawing4.addEventListener('click',()=>{
+   window.location.href = "./penitent-magdalene.html";
+   
+})
+Drawing5.addEventListener('click',()=>{
+   window.location.href = "./The-Storm-on-the-Sea-of-Galilee.html";
+
+})
+Drawing6.addEventListener('click',()=>{
+
+   window.location.href = "./the-great-wave-off-kanagawa.html";
+})
+Drawing7.addEventListener('click',()=>{
+
+   window.location.href = "./van-gogh-self-portrait.html";
+})
+Drawing8.addEventListener('click',()=>{
+
+   window.location.href = "./the-sleeping-gypsy.html";
+})
+Drawing9.addEventListener('click',()=>{
+
+   window.location.href = "./lady-with-an-ermine.html";
+})
+Drawing10.addEventListener('click',()=>{
+
+   window.location.href = "./the-night-cafe.html";
+})
+Drawing11.addEventListener('click',()=>{
+
+   window.location.href = "./mona-lisa.html";
+})
+Drawing12.addEventListener('click',()=>{
+
+   window.location.href = "./the-basket-of-apples.html";
+})
+Drawing13.addEventListener('click',()=>{
+
+   window.location.href = "./the-boy-in-the-red-vest.html";
+})
+Drawing14.addEventListener('click',()=>{
+
+   window.location.href = "./arnolfini-portrait.html";
+})
+Drawing15.addEventListener('click',()=>{
+
+   window.location.href = "./the-swing.html";
+})
